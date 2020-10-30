16293adb535b4d4782b0b0654ceace32f201fc1b
Dwi Fitriyanto <dwifitriyanto123@gmail.com>
2020-10-30 13:36:15 +0700

    changed format

--- js/app.js
+++ js/app.js
@@ -1,25 +1,25 @@
 $("#provinces").change(function () {
-    var id_provinces = $("#provinces").val();
-    $.ajax({
-        type: "POST",
-        dataType: "html",
-        url: "collect-data.php",
-        data: "provinces=" + id_provinces,
-        success: function (data) {
-            $("#regencies").html(data);
-        }
-    });
+  var id_provinces = $("#provinces").val();
+  $.ajax({
+    type: "POST",
+    dataType: "html",
+    url: "collect-data.php",
+    data: "provinces=" + id_provinces,
+    success: function (data) {
+      $("#regencies").html(data);
+    },
+  });
 });
 
 $("#regencies").change(function () {
-    var id_regencies = $("#regencies").val();
-    $.ajax({
-        type: "POST",
-        dataType: "html",
-        url: "collect-data.php",
-        data: "regencies=" + id_regencies,
-        success: function (data) {
-            $("#districts").html(data);
-        }
-    });
+  var id_regencies = $("#regencies").val();
+  $.ajax({
+    type: "POST",
+    dataType: "html",
+    url: "collect-data.php",
+    data: "regencies=" + id_regencies,
+    success: function (data) {
+      $("#districts").html(data);
+    },
+  });
 });
